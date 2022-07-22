0388f4244c98ea62deb38bb8614b94a22c2a7cc2
HotPr <sashashvaika@gmail.com>
2022-07-22 22:44:37 +0300

    Update code.py

--- code.py
+++ code.py
@@ -6,7 +6,22 @@ while True:
 
 
 
-    param = ['камень', 'бумага' , 'ножницы'] 
+    param = [
+        'камень',
+        'бумага' ,
+        'ножницы'
+        ]
+
+    
+    draw = ['Ничья!' , 'Никто не выиграл!']
+    win_player = ['Вы победили!', 'Компьютер проиграл!', 'Ты выиграл!']
+    
+    lose_player = [
+        'Вас выиграл компьютер!',
+        'Победил компьютер',
+        'Вы лооооооох!',
+        'Вас обыграла машина!'
+        ]
 
     print('Добро пожаловать в камень, ножницы, бумага!') #добавить время на выбор
     time.sleep(1)
@@ -25,28 +40,28 @@ while True:
 
 
     if people_answear == comp_answear:
-        print('Ничья!')
+        print(random.choice(draw))
         print('Компьютер тоже выбрал:', comp_answear)
     elif people_answear == 'бумага':
         if comp_answear == 'ножницы':
-            print('Победил компьютер!')
+            print(random.choice(lose_player))
             print('Компьютер выбрал:', comp_answear)
         elif comp_answear == 'камень':
-            print('Вы выиграли!')
+            print(random.choice(win_player))
             print('Компьютер выбрал:', comp_answear)
     elif people_answear == 'ножницы':
         if comp_answear == 'камень':
-            print('Победил компьютер!')
+            print(random.choice(lose_player))
             print('Компьютер выбрал:', comp_answear)
         elif comp_answear == 'бумага':
-            print('Вы выиграли!')
+            print(random.choice(win_player))
             print('Компьютер выбрал:', comp_answear)
     elif people_answear == 'камень':
         if comp_answear == 'бумага':
-            print('Победил компьютер!')
+            print(random.choice(lose_player))
             print('Компьютер выбрал:', comp_answear)
         elif comp_answear == 'ножницы':
-            print('Вы выиграли!')
+            print(random.choice(win_player))
             print('Компьютер выбрал:', comp_answear)
     else:
         print("Ошибка, вы не выбрали камень, ножницы или бумагу")
