42e1fef98646f34eb61ddb86744cd1cb6ff4b741
HotPr <sashashvaika@gmail.com>
2022-07-22 22:48:38 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,83 +1,41 @@
 import random
 import time
 
-
 while True:
 
+    final = {("каменьбумага", "бумаганожницы", "ножницыкамень"): ('Вас выиграл компьютер!', 'Победил компьютер', 'Вы лооооооох!', 'Вас обыграла машина!'),
+             ("каменькамень", "бумагабумага", "ножницыножницы"): ('Ничья!', 'Никто не выиграл!'),
+             ("каменьножницы", "ножницыбумага", "бумагакамень"): ('Вы победили!', 'Компьютер проиграл!', 'Ты выиграл!')}
 
-
-    param = [
-        'камень',
-        'бумага' ,
-        'ножницы'
-        ]
-
-    
-    draw = ['Ничья!' , 'Никто не выиграл!']
-    win_player = ['Вы победили!', 'Компьютер проиграл!', 'Ты выиграл!']
-    
-    lose_player = [
-        'Вас выиграл компьютер!',
-        'Победил компьютер',
-        'Вы лооооооох!',
-        'Вас обыграла машина!'
-        ]
-
-    print('Добро пожаловать в камень, ножницы, бумага!') #добавить время на выбор
+    print('Добро пожаловать в камень, ножницы, бумага!')  
     time.sleep(1)
-    print('Что ты выберешь: камень, ножницы или бумага?') #можно сделать рандомный текст победы
 
-    people_answear = input()
-
-
-    time.sleep(0.5)
-    print('Компьютер выбирает свой ответ...')
-    time.sleep(1)
-    print('...')
-
-    comp_answear = random.choice(param)
-
-
-
-    if people_answear == comp_answear:
-        print(random.choice(draw))
-        print('Компьютер тоже выбрал:', comp_answear)
-    elif people_answear == 'бумага':
-        if comp_answear == 'ножницы':
-            print(random.choice(lose_player))
-            print('Компьютер выбрал:', comp_answear)
-        elif comp_answear == 'камень':
-            print(random.choice(win_player))
-            print('Компьютер выбрал:', comp_answear)
-    elif people_answear == 'ножницы':
-        if comp_answear == 'камень':
-            print(random.choice(lose_player))
-            print('Компьютер выбрал:', comp_answear)
-        elif comp_answear == 'бумага':
-            print(random.choice(win_player))
-            print('Компьютер выбрал:', comp_answear)
-    elif people_answear == 'камень':
-        if comp_answear == 'бумага':
-            print(random.choice(lose_player))
-            print('Компьютер выбрал:', comp_answear)
-        elif comp_answear == 'ножницы':
-            print(random.choice(win_player))
-            print('Компьютер выбрал:', comp_answear)
-    else:
+    people_answear = input('Что ты выберешь: камень, ножницы или бумага - ')
+    if people_answear not in ("камень", "ножницы", "бумага"):
         print("Ошибка, вы не выбрали камень, ножницы или бумагу")
-    
-    print("\nВы хотите продолжить игру? \nда/нет" )
-    end_answear = input()
-    if end_answear == 'да':
-        print('Окей, продолжаем!')
-        print('\n -------------------- \n')
-    elif end_answear == 'нет':
-        print('Печалька, до скорой встречи!')
-        break
     else:
-        print('Вы не ответили, поэтому мини-игра будет закрыта через 5 секунд')
-        time.sleep(5)
-        break
-
-        
+        time.sleep(0.5)
+        print('Компьютер выбирает свой ответ...')
+        time.sleep(1)
+        print('...')
     
+        comp_answear = random.choice(("камень", "ножницы", "бумага"))
+    
+        for i in final.keys():
+            if people_answear + comp_answear in i:
+                print(random.choice(final[i]))
+                print('Компьютер выбрал:', comp_answear)
+
+    print("\nВы хотите продолжить игру? \nда/нет")
+    end_answear = input()
+    match input():
+        case "да":
+            print('Окей, продолжаем!')
+            print('\n -------------------- \n')
+        case "нет":
+            print('Печалька, до скорой встречи!')
+            break
+        case _:
+            print('Вы не ответили, поэтому мини-игра будет закрыта через 5 секунд')
+            time.sleep(5)
+            break
